06e4457146165d5560a8237cda914e247c6e0d03
jixuanyu <jixy@hzcominfo.com>
2019-09-10 15:40:43 +0800

    练习day1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="content">
+      <p>{{msg}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$color:red;
+h3 {
+  margin: 40px 0 0;
+}
+.content{
+  p{
+    color: $color;
+  }
+}
+</style>
